88c701b29e4a7a3d9d5f14fb55d0e8a826de2146
starplaton <starplaton@naver.com>
2021-10-26 06:13:16 +0900

    first commit

---
+++ calculation.js
@@ -0,0 +1,116 @@
+  
+export const accumulateTownArcherCost = (level: number): number => {
+  let result;
+  if (level < 22) {
+    result = 75 + (50 * (level - 1) * level) / 2;
+  } else if (level < 32) {
+    result = 10575 + ((level - 21) * (2000 + (level - 20) * 100)) / 2;
+  } else if (level < 37) {
+    result = 26075 + ((level - 31) * (4000 + (level - 30) * 200)) / 2;
+  } else if (level < 41) {
+    result = 39075 + ((level - 36) * (6000 + (level - 35) * 250)) / 2;
+  } else if (level < 46) {
+    result = 53575 + ((level - 40) * (8000 + (level - 39) * 300)) / 2;
+  } else if (level < 51) {
+    result = 78075 + ((level - 45) * (11000 + (level - 44) * 400)) / 2;
+  } else if (level < 54) {
+    result = 111575 + ((level - 50) * (15000 + (level - 49) * 500)) / 2;
+  } else {
+    result = 137075 + ((level - 53) * (18000 + (level - 52) * 1000)) / 2;
+  }
+  return Math.floor(result);
+};
+
+export const accumulateCastleCost = (level: number): number => {
+  let result;
+  if (level < 4) {
+    result = 1000;
+  } else if (level < 17) {
+    result = 950 + ((level - 3) * (1000 + (level - 2) * 250)) / 2;
+  } else if (level < 30) {
+    result = 34450 + ((level - 17) * (8500 + (level - 16) * 750)) / 2;
+  } else if (level < 60) {
+    result = 143950 + ((level - 29) * (26200 + (level - 28) * 1000)) / 2;
+  } else if (level < 1000) {
+    result = 1024200 + ((level - 58) * (18000 + (level - 57) * 3350)) / 2;
+  } else {
+    result =
+      1480836700 + ((level - 999) * (6087000 + (level - 998) * 2500)) / 2;
+  }
+  return Math.floor(result);
+};
+
+const under200: Readonly<number[]> = Array.from({
+  length: 199,
+})
+  .map((_, index) => index + 1)
+  .reduce(
+    (prev: number[], currentLevel) => [
+      ...prev,
+      currentLevel === 1
+        ? 0
+        : prev[currentLevel - 1] +
+          (Math.floor(currentLevel / 20) + 1) * currentLevel * 250,
+    ],
+    [0],
+  );
+const over200 = under200[199];
+const over5000 = over200 + 37447800000;
+const over10000 = over5000 + 149970000000;
+
+export const accumulateUnitCost = (
+  lv1_price: number,
+  level: number,
+): number => {
+  /*
+  This function is returning sum of below function about all levels.
+  const cost = (level1_price: number, current_level: number): number => {
+    if (current_level <= 0) {
+      return 0;
+    }
+    if (current_level === 1) {
+      return level1_price;
+    }
+    if (current_level < 200) {
+      return (Math.floor(current_level / 20) + 1) * current_level * 250;
+    }
+    if (current_level < 5001) {
+      return current_level * 3000;
+    }
+    if (current_level < 10000) {
+      return current_level * 4000;
+    }
+    return current_level * 5000;
+  }
+   */
+  if (level === 0) {
+    return 0;
+  }
+  if (level < 200) {
+    return lv1_price + under200[level - 1];
+  }
+  if (level < 5001) {
+    // not typo error. official formula
+    return lv1_price + over200 + ((level * (level + 1)) / 2 - 19900) * 3000;
+  }
+  if (level < 10000) {
+    return lv1_price + over5000 + ((level * (level + 1)) / 2 - 12502500) * 4000;
+  }
+  return lv1_price + over10000 + ((level * (level + 1)) / 2 - 49995000) * 5000;
+};
+
+export const accumulateGoldIncome = (wave: number): number => {
+  const waveIncome = ((wave * (wave + 1)) / 2) * 300;
+  const colonyIncome = 1364504 * wave * 5;
+  return waveIncome + colonyIncome;
+};
+export const getGoldLabel = (unit: number): string => {
+  if (unit > 0) {
+    let postfix = '';
+    for (let i = 0; i < unit; ++i) {
+      postfix += ',000';
+    }
+    return `${postfix} 골드`;
+  }
+  return '골드';
+};
