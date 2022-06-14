1daa045582efb1e4a670a0b002c7e6b848e47c3b
starplaton <starplaton@naver.com>
2022-06-15 05:02:50 +0900

    dd

--- helloworld.py
+++ helloworld.py
@@ -1,11 +1,11 @@
-n = int(input())
- 
-li = []
-for i in str(n):
-    li.append(int(i))
-# li = list(map(int,str(n))) 으로 변경가능
-    
-li.sort(reverse=True) # 내림차순
- 
-for i in li:
-    print(i,end='') 
+T = int(input())
+cute = 0
+
+for _ in range(T):
+    if int(input()) == 1:
+        cute += 1
+
+if cute > T//2:
+    print("Junhee is cute!")
+else:
+    print("Junhee is not cute!")
